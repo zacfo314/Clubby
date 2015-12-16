b5d1e7661727002cdfc79e1b3593808a9a9ede00
Tobias Kaulfuß <tobias.kaulfuss@denkwerk.com>
2015-12-16 18:39:08 +0100

    Refactoring

--- app/src/main/java/com/mathedia/clubby/ClubActivity.java
+++ app/src/main/java/com/mathedia/clubby/ClubActivity.java
@@ -2,8 +2,6 @@ package com.mathedia.clubby;
 
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -21,7 +19,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.view.ViewManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -40,19 +37,8 @@ import java.util.List;
 
 public class ClubActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private ViewPager mViewPager;
 
     @Override
@@ -77,26 +63,8 @@ public class ClubActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(mViewPager);
     }
 
-//    Menü wird von den Tabs befüllt
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_logout) {
-
-    public boolean logout() {
+    public boolean logout(boolean isClicked) {
+        if (isClicked) {
             ParseUser.logOutInBackground(new LogOutCallback() {
                 public void done(ParseException e) {
                     if (e == null) {
@@ -113,17 +81,16 @@ public class ClubActivity extends AppCompatActivity {
                     }
                 }
             });
-            return true;
+        }
+        return true;
     }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 
+    /*
+    *
+    * ADAPTER FÜR DIE TABS
+    *
+    */
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
@@ -132,15 +99,12 @@ public class ClubActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
             // gibt das entsprechende Fragment zurück.
             switch (position) {
                 case 0:
                     return DatesFragment.newInstance(position + 1);
-                case 1:
-                    return ClubsFragment.newInstance();
                 default:
-                    return PlaceholderFragment.newInstance(position + 1);
+                    return ClubsFragment.newInstance();
             }
         }
 
@@ -162,7 +126,12 @@ public class ClubActivity extends AppCompatActivity {
         }
     }
 
-    //Das Fragment, das die Termine anzeigt
+    /*
+    *
+    * TERMINE FRAGMENT
+    *
+    */
+
     public static class DatesFragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
@@ -207,7 +176,7 @@ public class ClubActivity extends AppCompatActivity {
             int id = item.getItemId();
 
             if (id == R.id.action_logout) {
-                ((ClubActivity)getActivity()).logout();
+                ((ClubActivity)getActivity()).logout(true);
             }
             return super.onOptionsItemSelected(item);
         }
@@ -223,10 +192,15 @@ public class ClubActivity extends AppCompatActivity {
         }
     }
 
-    //Das Fragment, das die Clubs anzeigt
+    /*
+    *
+    * CLUBS FRAGMENT
+    *
+    */
+
     public static class ClubsFragment extends ListFragment {
 
-        private static final ArrayList<String> clubIDs = new ArrayList<String>();
+        private static final ArrayList<String> clubIDs = new ArrayList<>();
 
         public ClubsFragment() {
         }
@@ -237,15 +211,30 @@ public class ClubActivity extends AppCompatActivity {
         }
 
         @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setHasOptionsMenu(true);
+        }
+
+        //@Override
+        //public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            //View rootView = inflater.inflate(R.layout.fragment_club, container, false);
+            //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            //textView.setText("Hier im Clubs fragment");
+            //return rootView;
+        //}
+
+        @Override
         public void onActivityCreated(Bundle savedInstanceState) {
             super.onActivityCreated(savedInstanceState);
 
-            //ArrayList<String> clubNameList = new ArrayList<String>();
+            // gets all clubs where current user is member
             final ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1);
             setListAdapter(adapter);
 
             ParseQuery<ParseObject> query = ParseQuery.getQuery("Club");
             query.whereEqualTo("users", ParseUser.getCurrentUser());
+
             query.findInBackground(new FindCallback<ParseObject>() {
                 public void done(List<ParseObject> clubList, ParseException e) {
                     if (e == null) {
@@ -256,31 +245,11 @@ public class ClubActivity extends AppCompatActivity {
                             adapter.add(clubName);
                             clubIDs.add(club.getObjectId());
                         }
-
                     } else {
                         Log.d("score", "Error: " + e.getMessage());
                     }
                 }
             });
-        }
-
-        @Override
-        public void onListItemClick(ListView l, View v, int position, long id) {
-            //String selection = l.getItemAtPosition(position).toString();
-            String selection = " "+position;
-            Toast.makeText(getActivity(), selection, Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(getActivity(), ClubMembersActivity.class);
-            //In extras wird der angeklickte Club gespeichert
-            intent.putExtra("clubID", clubIDs.get(position));
-            startActivity(intent);
-        }
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            setHasOptionsMenu(true);
-
         }
 
         @Override
@@ -291,65 +260,25 @@ public class ClubActivity extends AppCompatActivity {
 
         @Override
         public boolean onOptionsItemSelected(MenuItem item) {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
             int id = item.getItemId();
-
             if (id == R.id.action_logout) {
-                ((ClubActivity)getActivity()).logout();
+                ((ClubActivity)getActivity()).logout(true);
             }
-
             if (id == R.id.action_create_club) {
-
-                            Toast.makeText(getActivity(), "Club erstellt", Toast.LENGTH_LONG).show();
+                Toast.makeText(getActivity(), "Club erstellt", Toast.LENGTH_LONG).show();
                 return true;
             }
-
             return super.onOptionsItemSelected(item);
         }
 
-        //@Override
-        //public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            //View rootView = inflater.inflate(R.layout.fragment_club, container, false);
-            //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            //textView.setText("Hier im Clubs fragment");
-            //return rootView;
-        //}
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_club, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
+        public void onListItemClick(ListView l, View v, int position, long id) {
+            String selection = " "+position;
+            Toast.makeText(getActivity(), selection, Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(getActivity(), ClubMembersActivity.class);
+            //In extras wird der angeklickte Club gespeichert
+            intent.putExtra("clubID", clubIDs.get(position));
+            startActivity(intent);
         }
     }
 }
